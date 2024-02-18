dc96caa67fedd8205614f53d723ae053ad33989b
vitor kravszenko <vitorkravs3@gmail.com>
2024-02-18 00:40:22 -0300

    feat: aplicação dos componentes criados, input e page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import AuthInput from "@/components/AuthInput";
+import AuthPage from "@/components/AuthPage";
 import axios from "axios";
 import Link from "next/link";
 import { useState } from "react";
@@ -20,40 +22,20 @@ const Login = () => {
       });
   };
   return (
-    <main className="bg-[url('https://static.vecteezy.com/ti/vetor-gratis/p1/9982196-fundo-abstrato-botanico-para-paginas-da-web-banners-com-folhas-e-salpicos-fundo-editavel-vetor.jpg')] bg-no-repeat bg-cover flex min-h-screen flex-col items-center justify-center">
-      <form className="flex flex-col bg-white px-6 py-14 rounded-2xl gap-11 text-gray-600 w-1/4">
-        <h1 className="font-bold text-2x1 text-center">Login</h1>
-        <div className="flex flex-col justify-between items-start">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            onChange={(e) => setEmail(e.currentTarget.value)}
-            placeholder="Digite seu e-mail"
-            className="border-gray-400 border-b w-full focus-visible:border-gray-700 focus-visible:boder-b focus-visible:outline-none"
-          />
-        </div>
-        <div className="flex flex-col justify-between items-start">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.currentTarget.value)}
-            placeholder="Digite sua senha"
-            className="border-gray-400 border-b w-full focus-visible:border-gray-700 focus-visible:boder-b focus-visible:outline-none"
-          />
-        </div>
-        <button
-          className="bg-green-600 py-3  font-bold text-white rounded-lg hover:bg-green-800"
-          onClick={(e) => handleLogin(e)}
-        >
-          Entrar
-        </button>
-        <Link href="/register" className="text-center underline">
-          Cadastre-se
-        </Link>
-      </form>
-    </main>
+    <AuthPage>
+      <h1 className="font-bold text-2x1 text-center">Login</h1>
+      <AuthInput label="Email:" newState={setEmail} />
+      <AuthInput label="Password:" newState={setPassword} isPassword />
+      <button
+        className="bg-green-600 py-3  font-bold text-white rounded-lg hover:bg-green-800"
+        onClick={(e) => handleLogin(e)}
+      >
+        Entrar
+      </button>
+      <Link href="/register" className="text-center underline">
+        Cadastre-se
+      </Link>
+    </AuthPage>
   );
 };
 
